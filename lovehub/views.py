70eed76878751477a0a887992294a957191ee566
Vaishnavi Punde <vaishnavii.punde14@gmail.com>
2026-01-27 21:00:56 +0530

    Added love notes, important dates, love letters with bea

--- lovehub/views.py
+++ lovehub/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from datetime import date
 
-ALLOWED_USERS = ["Guddya", "guddu", "admin"]
+ALLOWED_USERS = ["guddya", "guddu", "admin"]
 
 
 def login_view(request):
